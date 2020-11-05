bd7f2248cd74d83022727d84ea67cd0ca9a23997
nur <nur.php@gmail.com>
2020-11-05 10:48:47 +0800

    fix(deploy): remove pom2 static serve

--- packages/frontend/ecosystem.config.js
+++ packages/frontend/ecosystem.config.js
@@ -1,17 +1,7 @@
 module.exports = {
   apps: [{
-      name: 'frontent-api',
-      script: './build/server.js',
-      instances: 1
-    },
-    {
-      name: 'frontent-static',
-      script: 'serve',
-      env: {
-        PM2_SERVE_PATH: '.build',
-        PM2_SERVE_PORT: 8080
-      },
-      instances: 1
-    }
-  ]
+    name: 'frontent-api',
+    script: './build/server.js',
+    instances: 1
+  }]
 }
