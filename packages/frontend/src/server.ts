272e30253d5a476a2a6e78d19a78f65edbc1fbf9
nur <nur.php@gmail.com>
2020-11-05 11:30:46 +0800

    feat(deploy): use express to serve static

--- packages/frontend/src/server.ts
+++ packages/frontend/src/server.ts
@@ -1,5 +1,7 @@
 
 import { createServer } from "http";
+import express from "express";
+
 
 import socketIo from "socket.io";
 import socketIoClient from "socket.io-client";
@@ -8,8 +10,11 @@ import { InstrumentStatsBroadcastMessage } from "@yoj/common";
 const PORT = process.env.PORT || 4001;
 const BROKER_HOST = process.env.BROKER_HOST || "ws://localhost:8900";
 
+const app = express();
+app.use(express.static('./build'));
+app.set("port", PORT);
+const server = createServer(app);
 
-const server = createServer();
 
 const io = socketIo(server);
 const brokerSocker = socketIoClient(BROKER_HOST, {
@@ -22,3 +27,5 @@ brokerSocker.on("instrument-stats", (instrument: InstrumentStatsBroadcastMessage
 });
 
 server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+
+
